e78d00764318c765129c7c469624af0f1f3f5b7f
Aesha7 <paesha27@gmail.com>
2022-06-16 11:39:21 -0600

    quotes

--- Final Project/DosePeriodViewController.swift
+++ Final Project/DosePeriodViewController.swift
@@ -18,8 +18,8 @@ class DosePeriodViewController: UIViewController, UIPickerViewDataSource, UIPick
     @IBOutlet weak var myPicker: UIPickerView!
     
     let pickerData = [
-        ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54"],
-        ["day(s)","weeks","months","years"]
+        ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54"],
+        ["None","day(s)","weeks","months","years"]
     ]
     
     let dayComponent = 0
@@ -74,6 +74,7 @@ class DosePeriodViewController: UIViewController, UIPickerViewDataSource, UIPick
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print(FullNameViewController.accountInfo)
         let addStr : String = number + " " + period
         FullNameViewController.accountInfo[4] = addStr
     }
--- Final Project/ViewController.swift
+++ Final Project/ViewController.swift
@@ -11,9 +11,30 @@ class ViewController: UIViewController {
     
     
 
+    @IBOutlet weak var quote: UILabel!
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var todayLabel: UILabel!
     @IBOutlet weak var fullDateLabel: UILabel!
+    
+    
+    @objc func randomQuoteGenerator() {
+        var quotesList = [String]()
+        quotesList.append("“Believe you can and you’re halfway there.” ~ Theodore Roosevelt")
+        quotesList.append("“Nothing is impossible. The word itself says: ‘I’m possible!'” ~ Audrey Hepburn")
+        quotesList.append("“You are never too old to set another goal or to dream a new dream.” ~ C.S. Lewis")
+        quotesList.append("“There is no failure except in no longer trying.” ~ Elbert Hubbard")
+        quotesList.append("“It Does Not Matter How Slowly You Go As Long As You Do Not Stop.” ~ Confucius")
+        quotesList.append("“I’m Not Telling You It Is Going To Be Easy, I’m Telling You It’s Going To Be Worth It.” ~ Arthur L. Williams")
+        quotesList.append("“When you feel like quitting think about why you started.” ~ Anonymous")
+        quotesList.append("“To accept ourselves as we are means to value our imperfections as much as our perfections.” ~ Sandra Bierig")
+        quotesList.append("“Part of courage is simple consistency.” ~ Peggy Noonan")
+        quotesList.append("“I have not failed, I have just found 10,000 ways that won’t work.” ~ Thomas Edison")
+        quotesList.append("“Every one of us needs to show how much we care for each other and, in the process, care for ourselves.” ~ Princess Diana")
+        quote.text = (quotesList[Int.random(in: 0..<11)])
+        
+    }
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +53,11 @@ class ViewController: UIViewController {
         
         fullDateLabel.text = dateFormatter.string(from: Date())
         
+        let timer : Timer?
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(randomQuoteGenerator), userInfo: nil, repeats: true)
+        }
+        
         // Do any additional setup after loading the view.
-    }
-
-
+    
 }
 
